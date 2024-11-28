1af74f26197f86825f21679ac0679a2d10372a48
ashis <ashishbaranwal@nitsolution.in>
2024-11-28 14:38:16 +0530

    rrrrr

---
+++ src/main/java/com/example/DevOps/project/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.DevOps.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @GetMapping("/msg")
+    public String getMsg() {
+        System.out.println("My name  is Ashish Baranwal and my friends call me Ash");
+        return "My name  is Ashish Baranwal and my friends call me Ash";
+    }
+}
